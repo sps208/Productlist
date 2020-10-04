9c3d6322f137102bdefe12edf44846b30893f140
sps208 <satriops20@gmail.com>
2020-10-04 19:12:55 +0700

    checpoint 9

---
+++ src/components/CardSection.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { View, StyleSheet } from 'react-native';
+
+const style = StyleSheet.create({
+  cardSection: {
+    padding: 5,
+    justifyContent: 'flex-start',
+    flexDirection: 'row'
+  }
+})
+
+class CardSection extends Component {
+    render(){
+      return(
+        <View style={ style.card }>
+         { this.props.children }
+        </View>
+      );
+    }
+}
+
+export default CardSection;
--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,15 +1,60 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
 
 import Card from './Card'
+import CardSection from './CardSection';
 
 class ProductItem extends Component {
+    
+    const style = StyleSheet.create({
+        productImage: {
+            flex: 1,
+            height: 300
+        },
+        productName: {
+            flex: 1,
+            fontSize: 18
+        },
+        productPrice: {
+            width: 100,
+            textAlign: 'right',
+            fontsize: 18,
+            color: 'navy' 
+        }
+
+    })
+
+
+    getPricedFormatted(price) {
+        if (price) {
+            const pricedFormatted = price
+            .toString()
+            .split('')
+            .reverse()
+            .reduce((a,c,i) => c + (i && i % 3 == 0 ? '.' : '') + a, '');
+            return `Rp${pricedFormatted}`;
+
+        }
+        return 'Rp0';
+    }
+
+
     render(){
+        const { name, image, price, actionlink } = this.props.product
+
       return(
         <Card>
-         <Text>
-         { this.props.product.name }
-         </Text>
+            <CardSection>
+                <Image style={ style.productImage } source= { { uri:image } } /> 
+            </CardSection>
+            <CardSection>
+                <Text  style={ style.productName }>
+                { name }
+                </Text>
+                <Text  style={ style.productPrice }>
+                    { this.getPricedFormatted(price) }
+                </Text>
+            </CardSection>
         </Card>
       );
     }
