c7a44b322319fac3a46e5568dec03a9063c73415
sps208 <satriops20@gmail.com>
2020-10-04 16:48:36 +0700

    checkpoint 6

--- App.js
+++ App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 import Header from './src/components/Header';
+import ProductList from './src/components/ProductList';
 
 class App extends Component {
     render(){
       return(
         <View>
           <Header title="Product list"/>
-          <Text>Hello World</Text>
+          <ProductList />
         </View>
       );
     }
---
+++ src/components/ProductList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+
+class ProductList extends Component {
+    render(){
+      return(
+        <View>
+          <Text>Product list </Text>
+        </View>
+      );
+    }
+}
+
+export default ProductList;
